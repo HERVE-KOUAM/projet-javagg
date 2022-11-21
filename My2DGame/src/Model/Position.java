0cd258ab12668784ca89bae1711c334f3a447d43
HERVE <HERVE@192.168.40.131>
2022-11-21 09:59:49 +0100

    gg

---
+++ My2DGame/src/Model/Position.java
@@ -0,0 +1,23 @@
+package Model;
+
+import View.ViewPanel;
+
+public class Position {
+
+	ViewPanel gp;
+	public Position(ViewPanel gp) {
+		this.gp = gp;
+	}
+	
+	/*public void
+	setObject() {
+		gp.obj[0] = new Diamond();
+		gp.obj[0].x = 9*gp.tileSize;
+		gp.obj[0].y = 0*gp.tileSize;
+		
+		gp.obj[1] = new Diamond();
+		gp.obj[1].x = 1*gp.tileSize;
+		gp.obj[1].y = 9*gp.tileSize;
+
+	}*/
+}
